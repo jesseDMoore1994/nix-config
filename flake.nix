112a8212ec953d65df9b33ca006bfa832641c1bc
Jesse Moore <jesse@jessemoore.dev>
2023-01-25 17:49:55 -0600

    update

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
               #./system-modules/sops
               ./system-modules/sound
               ./system-modules/steam
-              ./system-modules/tailscale
+              #./system-modules/tailscale
               ./system-modules/users
               ./system-modules/virtualization
               ./system-modules/xfce
