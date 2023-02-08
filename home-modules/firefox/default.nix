0f0e73f6777566c0a4f4084364f2e465f98deec6
Jesse Moore <jesse@jessemoore.dev>
2023-02-08 16:50:23 -0600

    update and minor fixes

--- home-modules/firefox/default.nix
+++ home-modules/firefox/default.nix
@@ -3,17 +3,17 @@
 {
   programs.firefox = {
     enable = true;
-    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      darkreader
-      firefox-color
-      ublock-origin
-      umatrix
-      vimium
-    ];
     profiles.default = {
       id = 0;
       name = "Default";
       isDefault = true;
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        darkreader
+        firefox-color
+        ublock-origin
+        umatrix
+        vimium
+      ];
     };
   };
 }
