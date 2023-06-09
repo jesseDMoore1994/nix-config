e34a5c4b182a35dfd275364b1ef44a5b87f1a02f
Jesse Moore <jesse@jessemoore.dev>
2023-06-09 16:44:56 -0500

    comment out font for kitty

--- home-modules/kitty/default.nix
+++ home-modules/kitty/default.nix
@@ -8,7 +8,7 @@ pkgs:
       dim_opacity = "1.0";
       background_opacity = "0.75";
       font_size = "12.0";
-      font_family = "Hasklug Nerd Font";
+      # font_family = "Hasklug Nerd Font";
     };
   };
 }
