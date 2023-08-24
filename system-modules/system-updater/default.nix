6474ec16cef19c57c0e473b3c18ba96a5896c425
Jesse Moore <jesse@jessemoore.dev>
2023-08-23 19:18:15 -0500

    testing system updater in prod

--- system-modules/system-updater/default.nix
+++ system-modules/system-updater/default.nix
@@ -24,8 +24,8 @@
       date >> /var/log/update_system.hs 2>&1
       ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .# >> /var/log/update_system.hs 2>&1
       date >> /var/log/update_user.hs 2>&1
-      ${pkgs.util-linux} runas jmoore bash -c "${pkgs.nix}/bin/nix build .#homeManagerConfigurations.jmoore@$(hostname).activationPackage"  >> /var/log/update_user.hs 2>&1
-      ${pkgs.util-linux} runas jmoore ./result/activate  >> /var/log/update_user.hs 2>&1
+      ${pkgs.util-linux}/bin/runuser jmoore bash -c "${pkgs.nix}/bin/nix build .#homeManagerConfigurations.jmoore@$(hostname).activationPackage"  >> /var/log/update_user.hs 2>&1
+      ${pkgs.util-linux}/bin/runuser jmoore ./result/activate  >> /var/log/update_user.hs 2>&1
 
       popd
     '';
