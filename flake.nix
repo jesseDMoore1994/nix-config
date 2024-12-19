7c540164be4afff0d379dbc159ae82ea2466669b
Jesse Moore <jesse.moore@adtran.com>
2024-12-19 10:07:01 -0600

    pin back home manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "jesse@jessemoore.dev NixOS configuration flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
     sops-nix.url = "github:Mic92/sops-nix";
